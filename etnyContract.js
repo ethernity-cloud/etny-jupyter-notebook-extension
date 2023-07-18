d2d80ec8e3175a81b99a1b818cdd545cc54f3174
Ciprian Florea <odyss3um@gmail.com>
2023-07-18 12:21:18 +0300

    added max price for task execution

--- etnyContract.js
+++ etnyContract.js
@@ -53,7 +53,7 @@ define(function (require, exports, module) {
     }
 
     const addDORequest = async (imageMetadata = IMAGE_HASH, payloadMetadata, inputMetadata, nodeAddress) => {
-        const cpu = 1, memory = 1, storage = 40, bandwidth = 1, duration = 60, instances = 1, maxPrice = 0;
+        const cpu = 1, memory = 1, storage = 40, bandwidth = 1, duration = 60, instances = 1, maxPrice = 10;
         return await etnyContract._addDORequest(cpu, memory, storage, bandwidth, duration, instances, maxPrice, imageMetadata, payloadMetadata, inputMetadata, nodeAddress);
     }
 
